265f49f64f4262ca0acac1d0ecb0d355d00056ff
SlaneR <black_scv@naver.com>
2019-01-27 18:14:59 +0900

    Build library structures

---
+++ TeamDEV.Utils.Tests.Forms/Form1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using TeamDEV.Utils.Forms;
+
+namespace TeamDEV.Utils.Tests.Forms {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            FormTable.Get<Form2>().label1.Text = "Accessing Form2.label1.Text on Form1";
+            FormTable.Get<Form2>().Show();
+        }
+    }
+}
